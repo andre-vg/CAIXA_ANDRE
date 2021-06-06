3a89571cecfd7ef9a25a1404b6c5d868f1759e94
André <avictorg@gmail.com>
2021-06-06 18:54:39 -0300

    telas pronta

--- caixa_registradora_ceub/PYTHON/links.py
+++ caixa_registradora_ceub/PYTHON/links.py
@@ -1,4 +1,4 @@
-import flask
+import sql
 import mysql.connector
 from flask import Flask, render_template, request
 from datetime import datetime
@@ -34,8 +34,9 @@ def login():
     cnx.close()
 
     if dados is not None:
-        nome = dados[0]
-        return render_template('index.html', nome_funcionario=nome)
+        global nome_funcionario
+        nome_funcionario = dados[0]
+        return render_template('index.html', nome_funcionario=nome_funcionario)
     else:
         return render_template('naoencontrado.html')
 
@@ -47,39 +48,61 @@ def principal():
     return render_template('formlogin.html', data=data)
 
 
-@app.route('/incluir/', methods=['POST'])
+@app.route('/incluir', methods=['POST'])
 def incluir():
     print('I got clicked!')
-    nome_cliente = request.form.get('nome_cliente')
-    endereco_cliente = request.form.get('endereco_cliente')
-    numero_cliente = request.form.get('numero_cliente')
-    cpf_cliente = request.form.get('cpf_cliente')
+    nme = request.form['nme']
+    end = request.form['end']
+    token = request.form['token']
+    cpf = request.form['cpf']
 
-    print(nome_cliente)
+    # Incluindo pessoa no SGBD
+    mysql = sql.SQL("qZAqwXH0Wi", "O387pnW1tb", "qZAqwXH0Wi")
+    comando = "INSERT INTO tb_cliente(nme_cliente, endereco_cliente, qtd_token, CPF) VALUES (%s, %s, %s, %s);"
+    if mysql.executar(comando, [nme, end, token, cpf]):
+        msg = "Cliente " + nme + " cadastrada com sucesso!"
+    else:
+        msg = "Falha na inclusão de pessoa."
 
-    cnx = mysql.connector.connect(user='qZAqwXH0Wi', password='O387pnW1tb',
-                                  host='remotemysql.com',
-                                  database='qZAqwXH0Wi')
+    return msg
 
-    print("Conectado=", cnx.is_connected())
-
-    cs = cnx.cursor()
-
-    sql = "INSERT INTO tb_cliente (nme_cliente, endereco_cliente, numero_cliente, CPF) " \
-          "VALUES (%s, %s, %s, %s);"
-    cs.execute(sql, (nome_cliente, endereco_cliente, numero_cliente, cpf_cliente))
-    cnx.commit()
-    cs.close()
-    cnx.close()
-
-    return render_template('incluir.html', nome_cliente=nome_cliente, endereco_cliente=endereco_cliente,
-                           numero_cliente=numero_cliente, cpf_cliente=cpf_cliente)
-
-@app.route('/pagar')
+@app.route('/pagar', methods=['POST'])
 def pagar():
     data = str(datetime.now())
 
-    return render_template('pagar.html', nome_funcionario=usuario, data=data)
+    
+    espresso=request.form['qtd_espresso']
+    torta=request.form['qtd_torta']
+    agua=request.form['qtd_agua']
+    vlr_espresso=int(espresso) * 2.00
+    vlr_torta=int(torta) * 5.00
+    vlr_agua=int(agua) * 1.00
+
+    
+
+    return render_template('pagar.html', nome_funcionario=nome_funcionario, data=data,
+                           espresso=espresso, torta=torta, agua=agua, vlr_torta=vlr_torta, vlr_espresso=vlr_espresso,
+                           vlr_agua=vlr_agua)
+
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+
+  # Recuperando dados vindos do Ajax
+  parte = request.form['parte']
+
+  # Incluindo pessoa no SGBD
+  mysql = sql.SQL("qZAqwXH0Wi", "O387pnW1tb", "qZAqwXH0Wi")
+  comando = "SELECT * FROM tb_cliente WHERE CPF LIKE CONCAT('%', %s, '%') ORDER BY CPF;"
+  cs=mysql.consultar(comando, [parte])
+  print(cs)
+  dados = cs.fetchone()
+  if dados == None:
+      saida = ""
+  else:
+      saida = str(dados[0]) + ',' + dados[1] + ',' + dados[2] + ',' + str(dados[3]) + ',' + dados[4]
+
+  return saida
 
 
 app.run(debug=True)
---
+++ caixa_registradora_ceub/PYTHON/sql.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+class SQL:
+   def __init__(self, usuario, senha, esquema):
+       self.cnx = mysql.connector.connect(user=usuario, password=senha,\
+                                          host='remotemysql.com',\
+                                          database=esquema)
+
+   def executar(self, comando, parametros):
+       cursor = self.cnx.cursor()
+       cursor.execute(comando, parametros)
+       self.cnx.commit()
+       cursor.close()
+       return True
+
+   def consultar(self, comando, parametros):
+       cursor = self.cnx.cursor()
+       cursor.execute(comando, parametros)
+       return cursor
+
+   def __del__(self):
+       self.cnx.close()
